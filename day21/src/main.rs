c2f66b5a9d0f973f615cbf84c2be24c7b44a7b80
Moukarram Kabbash <moukarram.kabbash@grohec.com>
2022-01-01 15:17:18 +0100

    part2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,109 +1,68 @@
-use itertools::*;
-
-struct Player {
-    score: usize,
-    current_space: usize,
-}
-struct Dice {
-    current: usize,
-    total_rolled: usize,
-}
+#[derive(Copy, Clone, Debug)]
 struct World {
-    players: [Player; 2],
-    dice: Dice,
+    players: [usize; 4],
+    current_player: usize,
+    possible_paths: u64,
 }
 
 impl World {
     fn new(space_1: usize, space_2: usize) -> Self {
         World {
-            players: [
-                Player {
-                    score: 0,
-                    current_space: space_1,
-                },
-                Player {
-                    score: 0,
-                    current_space: space_2,
-                },
-            ],
-            dice: Dice {
-                current: 0,
-                total_rolled: 0,
-            },
+            players: [space_1, 0, space_2, 0],
+            current_player: 0,
+            possible_paths: 1,
         }
     }
 
-    fn play_to_end(&mut self) -> usize {
-        let mut current_player = 0;
-        while !self.has_winner() {
-            let add = self.dice.next() + self.dice.next() + self.dice.next();
-            self.players[current_player].move_steps(add);
-
-            /*
-
-            println!(
-                "Player {} rolls {} and moves to space {} for a total score of {}.",
-                current_player + 1,
-                add,
-                self.players[current_player].current_space,
-                self.players[current_player].score
-            );
-             */
-            current_player = (current_player + 1) % 2;
-        }
-        self.loosing_score()
-    }
-
-    fn has_winner(&self) -> bool {
-        self.players.iter().any(Player::is_winner)
-    }
-
-    fn loosing_score(&self) -> usize {
-        if !self.has_winner() {
-            panic!("Not finished yet")
+    fn winner(&self) -> i8 {
+        if self.players[1] >= 21 {
+            //println!("Player 1 won {:?}", self.players);
+            return 0;
+        } else if self.players[3] >= 21 {
+            //println!("Player 2 won {:?}", self.players);
+            return 1;
         }
 
-        let looser = self.players.iter().find(|&x| !x.is_winner()).unwrap();
-        looser.score * self.dice.total_rolled
+        return -1;
     }
-}
-
-impl Player {
-    fn is_winner(&self) -> bool {
-        self.score >= 1000
-    }
-    fn move_steps(&mut self, steps: usize) {
-        self.current_space = (((self.current_space - 1) + steps) % 10) + 1;
-        self.score += self.current_space;
-    }
-}
-impl Dice {
-    fn next(&mut self) -> usize {
-        let mut next = self.current + 1;
-        if next > 100 {
-            next -= 100;
-        }
-        self.current = next;
-        self.total_rolled += 1;
-        next
+    fn move_steps(&mut self, steps: usize, possibilities: u64) -> i8 {
+        let pos = 2 * self.current_player;
+        self.players[pos] = ((self.players[pos] + (steps - 1)) % 10) + 1;
+        self.players[pos + 1] += self.players[pos];
+        self.current_player = (self.current_player + 1) % 2;
+        self.possible_paths *= possibilities;
+        self.winner()
     }
 }
 fn main() {
-    let mut world = World::new(4, 10);
-    let total_score = world.play_to_end();
-
-    println!("Totsl score {}", total_score);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn input_data_part2() {
-        let mut world = World::new(4, 8);
-        let total_score = world.play_to_end();
-
-        assert_eq!(total_score, 739785)
+    let mut possibilities = [0; 7];
+    let mut winning_paths: [u64; 2] = [0, 0];
+    for i in 0..3 {
+        for ii in 0..3 {
+            for iii in 0..3 {
+                possibilities[i + ii + iii] += 1;
+            }
+        }
     }
+    println!("possibilities {:?}.", possibilities);
+
+    //let mut worlds = vec![World::new(4, 8)];
+    let mut worlds = vec![World::new(4, 10)];
+
+    while let Some(current) = worlds.pop() {
+        //println!("next {:?}", current);
+        for i in 0..7 {
+            let mut next = current.clone();
+            match next.move_steps(i + 3, possibilities[i]) {
+                0 => winning_paths[0] += next.possible_paths,
+                1 => winning_paths[1] += next.possible_paths,
+                _ => worlds.push(next),
+            }
+            // println!("New {:?}.", next);
+        }
+        //println!("New queu {:?}", worlds.len());
+    }
+
+    println!("All {:?}.", winning_paths);
+    println!("Part 2 {:?}.", winning_paths.iter().max().unwrap());
 }
