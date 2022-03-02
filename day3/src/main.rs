adcc6412247c9b9a390a004d74617b6cf397027d
Moukarram Kabbash <mouk9000@hotmail.com>
2022-03-02 21:49:18 +0100

    refactor day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,16 @@
 use std::fs;
 use std::convert::TryFrom;
+use bitvec::prelude::*;
 
 fn main() {
     let base: i32 = 2;
 
 
     let content = fs::read_to_string("input.txt")
-    .expect("war richtig");
+    .expect("No input");
 
     let org = content
-    .split("\r\n")
+    .split("\n")
     .collect::<Vec<_>>();
 
     let positions = org.clone();
@@ -18,24 +19,14 @@ fn main() {
 
 
     fn bin_to_dec(bin: &str)-> i32{
-        let base: i32 = 2;
-        let chars = bin.chars().rev();
-        let mut p:u32 = 0;
-        let mut result = 0;
-        for c in chars {
-            if c == '1'{
-                result = result + base.pow(p)
-            }
-            p = p +1            
-        }
-        result
+        i32::from_str_radix(bin, 2).unwrap()
     }
     
-    fn recount(positions: Vec<&str>, w:usize)->([i32;WIDTH], [i32;WIDTH]){
+    fn count(positions: Vec<&str>)->([i32;WIDTH], [i32;WIDTH]){
         let mut zeros= [0;WIDTH];
         let mut ones= [0;WIDTH];
         for pos in positions {
-            assert_eq!(pos.len(), w); // fails
+            assert_eq!(pos.len(), WIDTH); // fails
             let mut i = 0;
             for c in pos.chars(){
                 if c == '1' {
@@ -49,55 +40,57 @@ fn main() {
         (zeros,ones)
     }
 
-    let (zeros, ones) = recount(positions, WIDTH);
-    
-    println!("ones is {:?}",ones);
-    println!("zeros is {:?}",zeros);
+    let (zeros, ones) = count(positions);
 
     let mut gamma  = 0;
     let mut epsilon   = 0;
     
     for  i in 0..WIDTH {
-        if ones[i] > zeros[i]{
+        if ones[i] > zeros[i]{                            
             gamma  += base.pow(u32::try_from(WIDTH -1 -i).unwrap())
         }else{
             epsilon  += base.pow(u32::try_from(WIDTH -1 -i).unwrap())
             
         }
+
     }
-    println!("Part1 {} * {} = {} ", gamma, epsilon, epsilon *  gamma);
+    let part1 =  epsilon *  gamma;
+    assert_eq!(part1, 852500);
+    println!("Part1 {} * {} = {} ", gamma, epsilon,part1);
     let mut oxygen = org.clone();
     let mut co2 = org.clone();
 
-    
-    println!("oxygen is {:?}",oxygen[0]);
-    println!("co2 is {:?}",co2[0]);
 
     for  i in 0..WIDTH {
-        let (zeros, ones) = recount(oxygen.clone(), WIDTH);
+        let (zeros, ones) = count(oxygen.clone());
         let expected =  if  ones[i] >= zeros[i] {'1'}else{'0'};
         oxygen = oxygen.iter().cloned().filter(|p| p.chars().nth(i).unwrap() == expected).collect();
-        println!("left {}",oxygen.len());
-        
-        println!("After filtering for {} oxygen is {:?}",expected, oxygen);
+
         if oxygen.len() == 1{
             break
         }
     }
     
     for  i in 0..WIDTH {
-        let (zeros, ones) = recount(co2.clone(), WIDTH);
+        let (zeros, ones) = count(co2.clone());
         let expected =  if  ones[i] >= zeros[i] {'0'}else{'1'};
         co2 = co2.iter().cloned().filter(|p| p.chars().nth(i).unwrap() == expected).collect();
-        println!("left {}",co2.len());
         if co2.len() == 1{
+
             break
         }
     }
 
+    let part2 = bin_to_dec(oxygen[0]) *bin_to_dec(co2[0]) ;
+    assert_eq!(bin_to_dec(oxygen[0]), 2235);
+    assert_eq!(bin_to_dec(co2[0]), 451);
+    assert_eq!(part2, 1007985);
     
     println!("oxygen is {:?} {}",oxygen, bin_to_dec(oxygen[0]));
     println!("co2 is {:?} {}",co2, bin_to_dec(co2[0]));
-    println!("Part2  {} ",  bin_to_dec(oxygen[0]) *bin_to_dec(co2[0]) );
+    println!("Part2  {} ",  part2);
+
+    
 
 }
+ 
